f6ec295a6152a545e851d9466d661e0dd1382e90
Lucas Henrique Serafim da Silva <165844637+LucasSerafim147@users.noreply.github.com>
2025-06-13 13:09:32 -0300

    Update api.ts

--- constants/api.ts
+++ constants/api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://192.168.0.107:3000/api",
+    baseURL: "http://localhost:3000/api",
 })
 
 api.interceptors.request.use(async (config) =>{
